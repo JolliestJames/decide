e12bbd6c9703cfb3dd3d6f94285f6d3acb2cbf7e
JolliestJames <cloudismydog@gmail.com>
2019-04-09 23:07:28 -0700

    Implement best_feature_to_split

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,7 @@
 import pickle
 import collections
 import math
+import operator
 
 with open("data", "rb") as f:
     L = pickle.load(f)
@@ -17,3 +18,21 @@ def entropy(data):
         return -1 * category_ratio * math.log(category_ratio, 2)
     
     return sum(item_entropy(category) for category in category_frequencies.values())
+
+def best_feature_to_split(data):
+    baseline_entropy = entropy(data)
+
+    def feature_entropy(f):
+        def e(v):
+            partitioned_data = [d for d in data if d[f] == v]
+            proportion = (float(len(partitioned_data))/float(len(data)))
+            return proportion * entropy(partitioned_data)
+        return sum(e(v) for v in set([d[f] for d in data]))
+
+    features = len(data[0]) - 1
+
+    information_gain = [baseline_entropy - feature_entropy(f) for f in range(features)]
+
+    best_feature, best_gain = max(enumerate(information_gain), key = operator.itemgetter(1))
+
+    return best_feature
