352e3d065c4d732216dccc0ea34f6b02b6d582a4
JolliestJames <cloudismydog@gmail.com>
2019-04-10 20:40:38 -0700

    Implement create_tree and classify

--- decision_tree.py
+++ decision_tree.py
@@ -24,15 +24,51 @@ def best_feature_to_split(data):
 
     def feature_entropy(f):
         def e(v):
-            partitioned_data = [d for d in data if d[f] == v]
-            proportion = (float(len(partitioned_data))/float(len(data)))
-            return proportion * entropy(partitioned_data)
+            partitioned = [d for d in data if d[f] == v]
+            proportion = (float(len(partitioned))/float(len(data)))
+            return proportion * entropy(partitioned)
         return sum(e(v) for v in set([d[f] for d in data]))
 
-    features = len(data[0]) - 1
+    feature_count = len(data[0]) - 1
 
-    information_gain = [baseline_entropy - feature_entropy(f) for f in range(features)]
+    information_gain = [baseline_entropy - feature_entropy(f) for f in range(feature_count)]
 
     best_feature, best_gain = max(enumerate(information_gain), key = operator.itemgetter(1))
 
     return best_feature
+
+def potential_leaf_node(data):
+    count = calculate_category_frequency(data)
+    return count.most_common(1)[0]
+
+def create_tree(data, label):
+    category, count = potential_leaf_node(data)
+    if count == len(data):
+        return category
+    node = {}
+    feature = best_feature_to_split(data)
+    feature_label = label[feature]
+    node[feature_label] = {}
+    classes = set([d[feature] for d in data])
+    for c in classes:
+        partitioned = [d for d in data if d[feature] == c]
+        node[feature_label][c] = create_tree(partitioned, label)
+    return node
+
+def root_node(tree):
+    return list(tree.keys())[0]
+
+def is_dictionary(potential_node):
+    return isinstance(potential_node, dict)
+
+def classify(tree, label, data):
+    root = root_node(tree)
+    node = tree[root]
+    index = label.index(root)
+
+    for key in node.keys():
+        if data[index] == key:
+            if is_dictionary(node[key]):
+                return classify(node[key], label, data)
+            else:
+                return node[key]
